296f635b3c1927476c7d3927b85b840e1a896f53
Volker Mische <volker.mische@gmail.com>
2018-08-15 01:16:13 +0200

    feat: init ipld

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 'use strict'
 
+const IpfsBlockService = require('ipfs-block-service')
+const IpfsRepo = require('ipfs-repo')
+const Ipld = require('ipld')
+const promisify = require('util').promisify
+
 const helpers = require('./helpers.js')
 const Node = require('./libp2pnode.js')
 
 const SERVER_PORT = 10333
 
+const initIpld = promisify((ipfsRepoPath, callback) => {
+  const repo = new IpfsRepo(ipfsRepoPath)
+  repo.init({}, (err) => {
+    if (err) {
+      callback(err)
+    }
+    repo.open((err) => {
+      if (err) {
+        callback(err)
+      }
+      const blockService = new IpfsBlockService(repo)
+      const ipld = new Ipld(blockService)
+      callback(null, ipld)
+    })
+  })
+})
+
 const main = async (argv) => {
+  const ipld = await initIpld('/tmp/ipfsrepo')
   const serverInfo = await helpers.createPeerInfo('./peerid-server.json',
     SERVER_PORT)
   const server = new Node({peerInfo: serverInfo})
